f29ec89dc46a13123d57c9d05c6fc68e9b0021e6
nabiilahputri13 <putrisafa1394@gmail.com>
2024-04-24 19:48:18 +0700

    [INCOMPLETE] Implement UserServiceImpl.

---
+++ src/main/java/id/ac/service/UserRegistrationRequest.java
@@ -0,0 +1,11 @@
+package id.ac.service;
+
+public class UserRegistrationRequest {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String role;
+
+    // Constructors, getters, setters, etc.
+}
